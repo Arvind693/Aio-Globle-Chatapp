1a03ffe6c9a62480a6d524d2e7817b394afe167f
Arvind kumar <97538697+Arvind693@users.noreply.github.com>
2024-11-04 11:00:27 +0530

    changing URL

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 // Initialize socket.io with the server
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: ['https://aio-globel-chatapp.onrender.com','http://localhost:3000'],
     methods: ['GET', 'POST'],
   },
 });
